80d860f4e4776fe95f32e90a7b409a67b1510c79
Ratanveer singh <126321833+Ratanvee@users.noreply.github.com>
2025-02-25 20:59:28 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import register, user_login, user_logout, dashboard, upload_document, home
+from django.urls import path
+# from .views import get_pending_orders, update_order_status
+# from .views import dashboard, redirect_to_owner_dashboard
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,5 +10,10 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
+    # path('dashboard/<int:user_id>/', dashboard, name='dashboard'),
     path('upload/<slug:unique_url>/', upload_document, name='upload_document'),
+    # path('dashboard/', redirect_to_owner_dashboard, name='redirect_dashboard'),
+    # path('dashboard/<str:username>/', dashboard, name='dashboard'),
+    # path('orders/pending/', get_pending_orders, name='get_pending_orders'),
+    # path('orders/update/<int:order_id>/', update_order_status, name='update_order_status'),
 ]
